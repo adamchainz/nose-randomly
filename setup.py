12fc9e941f8f1ee40eb7ce110c1ef181a2b82687
Adam Johnson <me@adamj.eu>
2018-02-16 11:06:24 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires=[
         'nose',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="BSD",
     zip_safe=False,
     keywords='nose, random, randomize, randomise, randomly',
